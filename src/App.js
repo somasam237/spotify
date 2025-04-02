cc041b3f657b1441179d6d1c716628bfef0c2633
somasam <samyra.tafawoung.mboufa@th-bingen.de>
2025-04-02 13:07:09 +0200

    The final update of the projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 //one import of react
 import React, { useEffect, useState } from "react";
-import login from "./login"; 
+import Login from "./components/Login";
+import Spotify from "./components/Spotify";
+import { reducerCases } from "./utils/Constants";
+import { useStateProvider } from "./utils/StateProvider";
  
 //now i import my function from the spotify so that when the user click on the login button
 //this new page shows up
-import { getTokenFromUrl } from "./Spotify";
 //import SpotifyWebApi from "spotify-web-api-js";
 //import Player from "./Player";
 //import { useDataLayerValue } from "./DataLayer";
 //import Header from "./Header";
 
 //const spotify = new SpotifyWebApi();
- function hello(){
-  return(
-    <h1> helloyou </h1>
-  );
- }
+
 function App() {
-  <h1> hello</h1>
+ /* <h1> hello</h1>
   //const [{ user, token }, dispatch] = useDataLayerValue();
   const [token, setToken] = useState();
   useEffect(() => {
@@ -42,8 +40,20 @@ function App() {
     <div className="App">
       {token ? <h1> Logged in</h1> : < login />}
     </div>
-  );
+  );*/
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      if (token) {
+        dispatch({ type: reducerCases.SET_TOKEN, token });
+      }
+    }
+    document.title = "Spotify";
+  }, [dispatch, token]);
+  return <div>{token ? <Spotify /> : <Login />}</div>;
 }
  
 
-export default hello;
+export default App;
